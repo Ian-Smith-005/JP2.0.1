0621e4c4c19335110b71c8c953f992a4c1248544
Santos Ivan <90592108+Ian-Smith-005@users.noreply.github.com>
2024-09-02 14:36:52 +0300

    error

---
+++ assets/js/redirect-error.js
@@ -0,0 +1,7 @@
+// Function to handle the redirection
+function redirectToError() {
+    window.location.href = 'error.html';
+}
+
+// Set a timer to call the redirect function after 2 seconds
+setTimeout(redirectToError, 2000);
